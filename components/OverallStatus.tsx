1ecb551a0d31b1e27e059176fb10005e9a55709a
wodebiji <ailiaobiji@gmail.com>
2025-05-17 20:55:24 +0800

    Update OverallStatus.tsx

--- components/OverallStatus.tsx
+++ components/OverallStatus.tsx
@@ -1,24 +1,19 @@
-import { Center, Title } from '@mantine/core'
+import { Center, Title, Box } from '@mantine/core'
 import { IconCircleCheck, IconAlertCircle } from '@tabler/icons-react'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 
 function useWindowVisibility() {
-  const [isVisible, setIsVisible] = useState(true);
-
+  const [isVisible, setIsVisible] = useState(true)
   useEffect(() => {
     const handleVisibilityChange = () => {
-      console.log('visibility change', document.visibilityState);
-      setIsVisible(document.visibilityState === 'visible');
-    };
-
-    document.addEventListener('visibilitychange', handleVisibilityChange);
-
+      setIsVisible(document.visibilityState === 'visible')
+    }
+    document.addEventListener('visibilitychange', handleVisibilityChange)
     return () => {
-      document.removeEventListener('visibilitychange', handleVisibilityChange);
-    };
-  }, []);
-
-  return isVisible;
+      document.removeEventListener('visibilitychange', handleVisibilityChange)
+    }
+  }, [])
+  return isVisible
 }
 
 export default function OverallStatus({
@@ -36,38 +31,50 @@ export default function OverallStatus({
     statusString = 'All systems operational'
     icon = <IconCircleCheck style={{ width: 64, height: 64, color: '#059669' }} />
   } else {
-    statusString = `Some systems not operational (${state.overallDown} out of ${state.overallUp + state.overallDown})`
+    statusString = `Some systems not operational (${state.overallDown} out of ${
+      state.overallUp + state.overallDown
+    })`
   }
 
   const [openTime] = useState(Math.round(Date.now() / 1000))
   const [currentTime, setCurrentTime] = useState(Math.round(Date.now() / 1000))
-  const isWindowVisible = useWindowVisibility();
+  const isWindowVisible = useWindowVisibility()
 
   useEffect(() => {
     const interval = setInterval(() => {
-      if (!isWindowVisible) {
-        return
-      }
+      if (!isWindowVisible) return
       if (currentTime - state.lastUpdate > 300 && currentTime - openTime > 30) {
-        // trigger a re-fetch
         window.location.reload()
       }
       setCurrentTime(Math.round(Date.now() / 1000))
     }, 1000)
-
     return () => clearInterval(interval)
   })
 
   return (
-    <>
+    <Box
+      style={{
+        maxWidth: 360,
+        margin: '-80px auto 0',
+        padding: '0 16px',
+        textAlign: 'center',
+      }}
+    >
       <Center>{icon}</Center>
-      <Title mt="sm" style={{ textAlign: 'center' }} order={1}>
+      <Title mt={6} order={1}>
         {statusString}
       </Title>
-      <Title mt="sm" style={{ textAlign: 'center', color: '#70778c' }} order={5}>
+      <Title
+        mt={4}
+        style={{
+          color: '#70778c',
+          whiteSpace: 'nowrap', // 保持在一行不换行
+        }}
+        order={5}
+      >
         Last updated on:{' '}
-        {`${new Date(state.lastUpdate * 1000).toLocaleString()} (${currentTime - state.lastUpdate} sec ago)`}
+        {`${new Date(state.lastUpdate * 1000).toLocaleString()} (${currentTime - state.lastUpdate} 秒前)`}
       </Title>
-    </>
+    </Box>
   )
 }
