ac29747797ee346dce663fc688951d24121ccf40
wodebiji <ailiaobiji@gmail.com>
2025-05-17 22:17:09 +0800

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
     <header className={classes.header}>
       <Container size="md" className={classes.inner}>
         <div>
-          <a href="https://www.youtube.com/@AM_CLUB" target="_blank">
+          <a href="" target="_blank">
             <Text size="xl" span>
               🕒
             </Text>
@@ -32,7 +32,7 @@ export default function Header() {
               variant="gradient"
               gradient={{ from: 'blue', to: 'cyan', deg: 90 }}
             >
-              服务监控
+              服务状态监控
             </Text>
           </a>
         </div>
